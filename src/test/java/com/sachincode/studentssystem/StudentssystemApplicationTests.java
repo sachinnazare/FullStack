aa8223d51ce5507c8e0aa0c62d54ae702fc20cc3
sachinnazare <sachinnazare58@gmail.com>
2023-10-01 04:22:05 -0700

    Initial commit

---
+++ src/test/java/com/sachincode/studentssystem/StudentssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachincode.studentssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
